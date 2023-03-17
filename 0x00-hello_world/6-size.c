7a48e3bc83bc7c1e7ba614f35849db25211e3167
abdirahmanmuktar <abdirahmanmuktar16@gmail.com>
2023-03-16 21:37:01 -0700

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,13 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
+
 printf("size of a char: %d byte(s)\n", sizeof(a));
 printf("size of an int: %d byte(s)\n", sizeof(b));
 printf("size of a long int: %d byte(s)\n", sizeof(c));
 printf("size of a long long int: %d byte(s)\n", sizeof(d));
 printf("size of a float: %d byte(s)\n", sizeof(f));
 return (0);
+
 }
+
