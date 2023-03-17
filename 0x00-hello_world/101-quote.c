577cf7165d439986352750c68688dcbb21969e91
abdirahmanmuktar <abdirahmanmuktar16@gmail.com>
2023-03-17 11:57:59 -0700

    simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,7 @@
  */
 int main(void)
 {
-
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-
+char s[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(s, sizeof(s), 1, stderr);
 return (1);
 }
