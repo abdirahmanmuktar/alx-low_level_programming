33ad4dd9fdd852008c9fa904b212a56a990b3316
abdirahmanmuktar <abdirahmanmuktar16@gmail.com>
2023-03-27 06:55:07 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @a: string
+ * Return: length
+ */
+int _strlen(char *a)
+{
+	int longi = 0;
+
+	while (*a != '0')
+	{
+		longi++;
+		a++;
+	}
+	return (longi);
+}
